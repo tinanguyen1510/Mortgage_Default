110cc4f1c0072bcf7945070fb6f176a4253ceb14
tinanguyen1510 <44903984+tinanguyen1510@users.noreply.github.com>
2020-05-04 15:37:53 -0400

    Commit

---
+++ Code/Archive - Pycharm format/1. Data Preprocessing.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+import os
+
+DIR = os.path.join( os.path.dirname(os.getcwd()))
+DATA_DIR = DIR + '\Data'
+SAMPLE_DIR = DATA_DIR + '\Sample Data'  # replace this with the directory of your raw data
+print(SAMPLE_DIR)
+print(os.listdir(SAMPLE_DIR))
+
+# SEPARATE PERFORMANCE AND TIME DATA INTO 2 DIFFERENT LISTS
+keyword = 'time'
+perf = []  # list containing all performance data - performance data has the word _time_
+orign = []  # list containing all origination data
+for f in os.listdir(SAMPLE_DIR):
+    if keyword in f:
+        perf.append(f)
+    else:
+        orign.append(f)
+
+print('Performance data: ',perf)
+print()
+print('Origination data:', orign)
+
+# data = pd.read_csv(os.path.join(SAMPLE_DIR, 'historical_data1_time_Q12013.txt'))
+# PREPROCESS PERFORMANCE DATA
+performance = []
+for file in perf:
+    filename = os.path.join(SAMPLE_DIR, file)
+    perf_df = pd.read_csv(filename, sep="|", usecols=[0,1,3], names=['id_loan', 'mthly_rpt', 'default'], skipinitialspace=True, error_bad_lines=False,
+                              index_col=False, dtype='unicode') # only select specific columns to save computing time
+    perf_df['default'] = perf_df['default'].replace('XX', np.NaN) # convert XX values in default column to NaN
+    perf_df['default'] = perf_df['default'].replace('R', np.NaN) # convert R values in default column to NaN
+    perf_df.dropna(inplace=True)
+    perf_df = perf_df[(perf_df['default'] != '0') & (perf_df['default'] != '1') & (perf_df['default'] != '2')]  # only select default > 3 (more than 90 days delinquent D90)
+    perf = pd.DataFrame(np.unique(perf_df['id_loan']), columns=['id_loan'])  # find unique id loan with the above criteria
+    performance.append(perf)
+
+master_perf = pd.concat(performance)  # a dataframe containing all loan ids with D90 or worse
+print('Shape of performance data: ', master_perf.shape)
+print(master_perf.head())
+
+# PREPROCESS ORIGINATION DATA
+origination = []
+for file in orign:
+    filename = os.path.join(SAMPLE_DIR, file)
+    orign_df = pd.read_csv(filename, sep="|",
+                              names=['cr_scr', 'frst_pmt', 'frst_homebuyer', 'mtry_date', 'MSA', 'MI_pct', 'unit','occ_sts', 'cltv', 'dti','upb',
+                 'ltv','interest_rate', 'channel', 'ppm','pdt_type','ppty_state','ppty_type','pstl_code','id_loan','loan_prps',
+                 'term','total_borr','slr','srvc','cnfm_flag'], skipinitialspace=True, error_bad_lines=False,
+                              index_col=False, dtype='unicode') # import text files, include all data
+    # replace placeholder values with NaN and drop NaN
+    orign_df[['cr_scr', 'ltv', 'dti', 'interest_rate', 'cltv', 'MI_pct', 'upb']] = orign_df[['cr_scr', 'ltv', 'dti', 'interest_rate', 'cltv', 'MI_pct', 'upb']].astype('float64')
+    orign_df['cr_scr'] = [np.NaN if x == 9999 else x for x in (orign_df['cr_scr'].apply(lambda x: x))]
+    orign_df['frst_homebuyer'] = [np.NaN if x == '9' else x for x in (orign_df['frst_homebuyer'].apply(lambda x: x))]
+    orign_df['MI_pct'] = orign_df['MI_pct'].astype('int64')
+    orign_df['interest_rate'] = orign_df['interest_rate'].astype(float)
+    orign_df['MI_pct'] = [np.NaN if x == 999 else x for x in (orign_df['MI_pct'].apply(lambda x: x))]
+    orign_df['unit'] = [np.NaN if x == 99 else x for x in (orign_df['unit'].apply(lambda x: x))]
+    orign_df['occ_sts'] = [np.NaN if x == 9 else x for x in (orign_df['occ_sts'].apply(lambda x: x))]
+    orign_df['cltv'] = [np.NaN if x == 999 else x for x in (orign_df['cltv'].apply(lambda x: x))]
+    orign_df['dti'] = [np.NaN if (x == 9999 or x == 999) else x for x in (orign_df['dti'].apply(lambda x: x))]
+    orign_df['cnfm_flag'] = orign_df['cnfm_flag'].fillna('N')
+    orign_df.drop(['ppm', 'pdt_type'], axis=1,  inplace=True)
+    orign_df.dropna(inplace=True)
+    origination.append(orign_df)
+
+master_orign = pd.concat(origination) # dataframe with all origination data
+print('Shape of origin data: ', master_orign.shape)
+print()
+print('Total loans funded: ', master_orign.shape[0])
+print()
+
+# MASTER DATAFRAME PREPROCESSING
+master_df = master_perf.merge(master_orign, on='id_loan', how='outer', indicator=True)  # merge performing and origination data
+# drop all remanining NaN values
+master_df = master_df.dropna()
+
+# create year and Year+Quarter columns by extracting from columns loan id
+master_df['Year'] = ['20' + x for x in (master_df['id_loan'].apply(lambda x: x[2:4]))]
+master_df['YrQtr'] = ['20' + x for x in (master_df['id_loan'].apply(lambda x: x[2:6]))]
+# create a default column. Loans in both origination and D90 perf dataframes are delinquent. Loans only in origination are not delinquent
+master_df['default'] = master_df['_merge'].map({'both': 1, 'right_only': 0})
+
+# Merge w quarterly interest rate
+int_rate = pd.read_excel(os.path.join(DATA_DIR + '\\30-fixed-rates.xlsx'), sheet_name='Qtr Avg')
+int_rate['Rate'] = int_rate['Rate'].astype(float)
+# print('Total rows for quarterly interest rate: ', int_rate.shape[0])
+master_df = master_df.merge(int_rate, on='YrQtr', how='left')
+print('Final df shape: ', master_df.shape)
+# create a variable to indicate the spread of interest rate at origination (30-year fixed-rate less rate of the loan)
+master_df['sato'] = master_df['Rate'] - master_df['interest_rate']
+print('Number of good and bad loans: ', '\n', master_df['default'].value_counts()) # count delinquent vs good loans
+print()
+print('Total number of unique loans: ', len(np.unique(master_df['id_loan'])))
+
+# master_df.to_csv('C:/Users/tina/Desktop/Capstone/Data/d90_full_data.csv', index=False) (full dataset)
+
+master_df.to_csv(os.path.join(DATA_DIR + '\d90_full_population.csv'), index=False) # generate sampled dataset
+
---
+++ Code/Archive - Pycharm format/2. Modeling.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import numpy as np
+import os
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import *
+import time
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
+from xgboost import XGBClassifier
+import matplotlib.pyplot as plt
+from collections import Counter
+
+# check your working directory
+DIR = os.path.join( os.path.dirname(os.getcwd()))
+print(DIR)
+DATA_DIR = DIR + '\DATS 6501 - Capstone Project - Tina Nguyen\Data'
+print(DATA_DIR)
+
+# IMPORT DATA
+
+master_df = pd.read_csv(os.path.join(DATA_DIR, 'd90_full_population.csv'))
+print(master_df.shape)
+print(master_df.columns)
+
+# PREPARING DATA
+
+# drop unnecessary columns (identifier, time, location, etc.)
+X = master_df.drop(['default','_merge','Year','id_loan','frst_pmt','mtry_date','MSA','pstl_code', 'YrQtr', 'Rate', 'slr', 'srvc'], axis=1)
+y = master_df['default']
+
+# summary statistics
+stat = X[['ltv', 'dti', 'sato', 'interest_rate','cr_scr','MI_pct']].describe()
+stat.to_csv(os.path.join(DATA_DIR, 'full population summary statistics.csv'))
+
+# convert categorical variables to dummy
+X = pd.get_dummies(X)
+# standardize the data
+ss = StandardScaler()
+X = pd.DataFrame(ss.fit_transform(X), columns=X.columns)
+# use LabelEncoder to encode the default variable
+class_le = LabelEncoder()
+y = class_le.fit_transform(y)
+# split the data into training and testing set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+
+# resample the training dataset to different sizes
+undersample50 = RandomUnderSampler(sampling_strategy=0.5)
+X_under50, y_under50 = undersample50.fit_resample(X_train, y_train)
+
+# oversample 20%, undersample 30%
+oversample = RandomOverSampler(sampling_strategy=0.2)
+undersample50 = RandomUnderSampler(sampling_strategy=0.3)
+X_over, y_over = oversample.fit_resample(X_train, y_train)
+X_combine, y_combine = undersample50.fit_resample(X_over, y_over)
+
+# # plot class distribution for each population
+# def plotData(data):
+#     c = Counter(data)
+#     plt.bar(c.keys(), c.values(), color = ['blue', 'orange'])
+#     plt.show()
+#
+#
+# for i in [y_train, y_under50, y_combine]:
+#     plotData(i)
+#     plt.show()
+
+# TRAINING
+
+# function to print out the score of each iteration
+def prediction_score(clf_name,session_name,y, pred):
+    print(clf_name,'\n',session_name,'\n Accuracy:',accuracy_score(y, pred),
+         'ROC AUC:',roc_auc_score(y, pred), '\n',
+         'Precision Recall fscore support',precision_recall_fscore_support(y, pred, average='macro', labels=np.unique(pred)),
+          '\n', confusion_matrix(y, pred))
+
+
+# function to declare and train different models
+def classification_balanced(train_X, train_y, test_X, test_y):
+    # declare the classifers
+    lr_clf = LogisticRegression(random_state=42)
+    xg_clf = XGBClassifier(random_state=42)  #(learning_rate=0.05, n_estimators=300, max_depth=5)
+    dt_clf = DecisionTreeClassifier(random_state=42)
+    start_time = time.time()
+    for clf in (lr_clf, dt_clf, xg_clf):
+
+        clf.fit(train_X, train_y) # fit the model
+        y_pred_train = clf.predict(train_X)  # make preidiction on the training set
+        y_pred_test = clf.predict(test_X)  # make prediction on the testing set
+
+        # apply the scoring function
+        prediction_score(clf.__class__.__name__, 'Training', train_y, y_pred_train)
+        prediction_score(clf.__class__.__name__, 'Testing', test_y, y_pred_test)
+
+        print()
+        print('-----------------------------------------------------')
+        print()
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+# fit the model on the full population X_train, y_train
+print('BASE MODEL:')
+classification_balanced(X_train, y_train, X_test, y_test)
+print('___________________________________________________________________')
+
+# fit the model on reduced data
+print('UNDERSAMPLING 50%:')
+classification_balanced(X_under50, y_under50, X_test, y_test)
+print('___________________________________________________________________')
+
+# fit the model on combined data
+print('COMBINE:')
+classification_balanced(X_combine, y_combine, X_test, y_test)
+print('___________________________________________________________________')
+
+
+
+
+
